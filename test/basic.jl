67e4e3eb632e6307656d10928c3b10b9f4d06546
Oliver <oliver@dtu>
2014-05-12 11:36:13 +0200

    Generated TeX and PDFs

--- test/basic.jl
+++ test/basic.jl
@@ -4,7 +4,7 @@ x = linspace(0,2pi,100);
 fig = plt.figure();
 plot(x,sin(x),color="red");
 title("Test plot");
-xlabel(L"x");
+xlabel(L"$x$");
 ylabel(L"$\sin(x)$");
 printfig(fig)	# Saves to file "FIG1.tex"
 printfig(fig,filename="test.tex") # Saves to file "test.tex"
